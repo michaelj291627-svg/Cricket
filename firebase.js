e72553f86d3ba4d300caf27244490b0ad986f45a
michaelj291627-svg <michaelj291627@gmail.com>
2026-09-07 15:03:07 +0530

    Initialize Firebase app and Firestore database

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALEQvahvUUg0g99MNMVBguP7eQrKhMhvg",
+  authDomain: "cricket-33fb3.firebaseapp.com",
+  projectId: "cricket-33fb3",
+  storageBucket: "cricket-33fb3.firebasestorage.app",
+  messagingSenderId: "548674659187",
+  appId: "1:548674659187:web:b47fd1d33fe15d8be95b06"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
